9807c6699d350f4ea540593cbc4dfd68bb059b3d
RosaDamascena <hsj990604@gmail.com>
2023-10-18 22:57:35 +0900

    1018

--- Basic/1546_Average/main.cpp
+++ Basic/1546_Average/main.cpp
@@ -3,15 +3,22 @@ using namespace std;
 
 int main() {
   int N;
+  int M = -1;
   cin >> N;
-  int a;
-  int sum_N;
+  int score;
+  int sum = 0;
+  float answer;
 
-  for (int i = 0; i < N; i++)
-  {
-    cin >> a;
-    sum_N += a;
+  for (int i = 0; i < N; i++) {
+    cin >> score;
+    sum += score;
+    if (score > M) {
+      M = score;
+    }
   }
-  cout << sum_N / N;
+
+  answer = sum * 100.0 / M / N;
+  cout << answer;
+
   return 0;
 }
