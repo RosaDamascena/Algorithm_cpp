17b87892c4d6e05d222b8aee2ddfc899a3d8641a
RosaDamascena <hsj990604@gmail.com>
2025-01-19 23:16:08 +0900

    Solve: 플로이드워셜을 활용한 경로 찾기

---
+++ BOJ/11403_FindingPath/main.cpp
@@ -0,0 +1,40 @@
+// 11403 - 경로 찾기
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<vector<int>> graph(N, vector<int>(N));
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    // 플로이드-워셜 알고리즘
+    for (int k = 0; k < N; k++) { 
+        for (int i = 0; i < N; i++) { 
+            for (int j = 0; j < N; j++) {
+                if (graph[i][k] == 1 && graph[k][j] == 1) {
+                    graph[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
